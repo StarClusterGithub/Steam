4765449e171202205ede8e1ddbda8abd6a9b55ec
kuranadomoe <Wearestardust404@outlook.com>
2017-02-02 18:26:34 +0800

    mod infomation

---
+++ Don't Starve mod/Kotomi Ichinose/modinfo.lua
@@ -0,0 +1,25 @@
+-- This information tells other players more about the mod
+name = "Kotomi Ichinose"
+description = "Clannad anime Kotomi Ichinose character mod. This is one of my favorite characters"
+author = "Star Cluster"
+version = "Public preview version"
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = "um......I don't know..."
+api_version_dst = "↑↑↑"
+
+-- Compatible with the base game, RoG, SW, and DST
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+dst_compatible = true
+
+--whether need restart
+restart_required = false
+
+-- Can specify a custom icon for this mod!
+icon_atlas = ""
+icon = ""
+
+-- Specify the priority
+priority=-1
