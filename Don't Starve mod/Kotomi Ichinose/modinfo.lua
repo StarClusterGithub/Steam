9d07225aef1de1a2629d55e30d32fe38a5e0f987
kuranadomoe <Wearestardust404@outlook.com>
2017-02-03 23:42:11 +0800

    standard

--- Don't Starve mod/Kotomi Ichinose/modinfo.lua
+++ Don't Starve mod/Kotomi Ichinose/modinfo.lua
@@ -1,25 +1,37 @@
 -- This information tells other players more about the mod
+
+
+--The name of the mod displayed in the 'mods' screen.
 name = "Kotomi Ichinose"
+
+--A description of the mod.
 description = "Clannad anime Kotomi Ichinose character mod. This is one of my favorite characters"
+
+--Who wrote this awesome mod?
 author = "Star Cluster"
-version = "Public preview version"
 
--- This lets other players know if your mod is out of date, update it to match the current version in the game
-api_version = 6         --"um......I don't know..."
-api_version_dst = 10    --"↑↑↑"
+--A version number so you can ask people if they are running an old version of your mod.
+version = "1.0"
 
--- Compatible with the base game, RoG, SW, and DST
+--This lets other players know if your mod is out of date.  This typically needs to be updated every time there's a new game update.
+api_version = 6
+
+-- Compatible with both the base game and reign of giants
 dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
 dst_compatible = true
 
---whether need restart
-restart_required = false
+--Some mods may crash or not work correctly until the game is restarted after the mod is enabled/disabled
+restart_required = true
 
--- Can specify a custom icon for this mod!
+-- Set this to true to prevent _ANY_ other mods from loading while this mod is enabled.
+standalone = true
+
 icon_atlas = "test.xml"
 icon = "test.tex"
 
+forumthread = ""
+
 -- Specify the priority
-priority=-1
+--priority=-1
