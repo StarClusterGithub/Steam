6e599971f5ee2eea0ae669efd661aa8772e244a1
kuranadomoe <Wearestardust404@outlook.com>
2017-02-07 16:46:34 +0800

    comment

--- Don't Starve mod/Kotomi Ichinose/scripts/kotomi_speech.lua
+++ Don't Starve mod/Kotomi Ichinose/scripts/kotomi_speech.lua
@@ -1,5 +1,5 @@
 --[[
-	--- This is Wilson's speech file for Don't Starve Base ---
+	--- This is Wendy's speech file for Don't Starve Base ---
 	Write your character's lines here. If you want to use another speech file as a base, get them from data\scripts\
 	
 	If you have the DLC and want custom lines for those, use a speech file from data\DLC0001\scripts instead.
